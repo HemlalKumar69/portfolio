59dcae41dea01aa7f02850988df676e17b43cb82
Hemlal Kumar <hemlalkumar69@gmail.com>
2025-12-31 11:20:43 +0530

    Fix Render blank issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  base: "./",   // 🔥 THIS LINE FIXES RENDER ISSUE
+  plugins: [
+    react(),
     tailwindcss(),
   ],
 })
